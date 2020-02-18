4d485b071b703440fcd7ef4325b66e6e95b43c74
Arty Barnabi <rbarnabi676@gmail.com>
2020-02-18 09:42:58 -0800

    added base models and db config

--- primo/models.py
+++ primo/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Game(models.Model):
+    name = models.charField(max_length=256)
+    publisher = models.charField(max_length=100)
+    Genre = models.charField(max.length=100)
+
+
+class Guide(models.Model):
+    title = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='guides')
+    skill_level = models.charField(max_length=5)
+    content = models.TextField()
+
+# tags added as a different class with a related field back to guides?
+# blog posts
+# favorites
--- primo_django/settings.py
+++ primo_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'primo',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'primo_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'primo_db',
+        'USER': 'primo_admin',
+        'PASSWORD': 'dfNn6yWTQk4N',
+        'HOST': 'localhost'
     }
 }
 
