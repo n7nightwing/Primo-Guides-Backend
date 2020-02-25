82b4d3f4eaf21ab96aea869f5139cdc381d39a14
Arty Barnabi <rbarnabi676@gmail.com>
2020-02-25 00:38:25 -0800

    even more fixes for deploy

--- Procfile
+++ Procfile
@@ -1,4 +1,5 @@
 release: python manage.py migrate
 heroku ps:scale web=1
 web: gunicorn primo_django.wsgi --log-file -
+web: python manage.py runserver 0.0.0.0:$PORT --noreload
 
--- primo_django/settings.py
+++ primo_django/settings.py
@@ -30,7 +30,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost',
-                 '127.0.0.1',            'primo-guides.herokuapp.com']
+                 '127.0.0.1', 'primo-guides.herokuapp.com']
 
 
 # Application definition
